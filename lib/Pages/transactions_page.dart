5cecd47960a4fa560c9f35494cfb6b07e19d3be1
Vladimir Grigore <vgrigore@gmail.com>
2019-03-20 11:53:25 -0700

    Finish transactions list page design

--- lib/Pages/transactions_page.dart
+++ lib/Pages/transactions_page.dart
@@ -19,14 +19,13 @@ class _TransactionsPage extends State<TransactionsPage> {
     return new FutureBuilder(
       future: getTransactions(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if(snapshot.hasData) {
+        if(snapshot.hasData) {    
           if(snapshot.data != null) {
             var transactions  = snapshot.data['data'];
 
             return new Container(
               child: ListView.builder (
                 itemCount: transactions.length,
-                padding: EdgeInsets.all(8.0),
                 itemBuilder: (BuildContext context, int index) {
                   if(transactions != null) {
                     return TransactionListItem(transactions:transactions, index: index);
@@ -50,7 +49,6 @@ class TransactionListItem extends StatelessWidget {
   final int index;
 
   TransactionListItem({Key key, this.transactions, this.index}) : super(key: key);
-  
 
   @override
   Widget build(BuildContext context) {
@@ -63,37 +61,67 @@ class TransactionListItem extends StatelessWidget {
               padding: const EdgeInsets.all(16),
               child: Container(
                 width: MediaQuery.of(context).size.width,
-                child: Text("${formatDate(index)}"),
+                child: Text(formatDate(index, "EEEE MMMM d, y")),
               ),
             ),
           ),
-          Card(
-            child:Padding(
-              padding: const EdgeInsets.all(16),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                child: Text("${transactions[index]['attributes']['merchant-name']}"),
-              ),
-            ),
-          )
+          _transaction(),
         ],
       );
     } else {
-      return Card(
-        child:Padding(
-          padding: const EdgeInsets.all(16),
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            child: Text("${transactions[index]['attributes']['merchant-name']}"),
-          ),
-        ),
-      );
+      return _transaction();
     }
   }
 
-  shouldDisplayHeader(index) {
+  Widget _transaction () {
+    var amount = NumberFormat.currency(locale: "en_US", symbol: "\$");
+    var type = transactions[index]['attributes']['reversal'] ? "Refund" : "Payment";
+
+    return Card(
+      child: InkWell(
+        onTap: () => { print("Transaction row tapped") },
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Flexible(
+                child: Padding(
+                  padding: EdgeInsets.only(right: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                        Text(
+                          "${transactions[index]['attributes']['merchant-name']}",
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(formatDate(index, "jm")),
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                        Text(amount.format(transactions[index]['attributes']['amount']['cents']/100)),
+                        Text(type),
+                    ],
+                  ),
+                  Icon(Icons.chevron_right),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  bool shouldDisplayHeader(index) {
     if(index > 0) {
-      if(formatDate(index)  == formatDate(index - 1)) {
+      if(formatDate(index, "EEEE MMMM d, y")  == formatDate(index - 1, "EEEE MMMM d, y")) {
         return false;
       } else {
         return true;
@@ -103,11 +131,10 @@ class TransactionListItem extends StatelessWidget {
     }
   }
 
-  formatDate(index) {
+  String formatDate(index, format) {
     var transactionDate = transactions[index]['attributes']['created-at'];
     DateTime date = DateTime.parse(transactionDate);
-    String formattedDate = DateFormat("EEEE MMMM d, y").format(date);
+    String formattedDate = DateFormat(format).format(date);
     return formattedDate;
   }
-
 }
