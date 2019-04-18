9e8aef89df20faa492cf5a453112e03671cf27f1
Vladimir Grigore <vgrigore@gmail.com>
2019-04-18 14:55:15 -0700

    Finish design for featured offers carousel

--- lib/Components/featured_offers.dart
+++ lib/Components/featured_offers.dart
@@ -3,7 +3,7 @@ import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:flutter_page_indicator/flutter_page_indicator.dart';
 
 class FeaturedOffers extends StatelessWidget {
-  List featuredOffers;
+  final List featuredOffers;
   FeaturedOffers({ Key key, this.featuredOffers}) : super(key: key);
 
   @override
@@ -14,12 +14,51 @@ class FeaturedOffers extends StatelessWidget {
         itemBuilder: (BuildContext context, int index) {
           return Padding(
             padding: EdgeInsets.only(bottom: 30.0, left: 15.0, right: 15.0),
-              child: ClipRRect(
-              borderRadius: BorderRadius.circular(10.0),
-              child: Image.network(
-                featuredOffers[index]['attributes']['image']['default'],
-                fit: BoxFit.fitWidth,
-              ),
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Image.network(
+                    featuredOffers[index]['attributes']['image']['default'],
+                    fit: BoxFit.fitWidth,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.center,
+                        colors: [Colors.black.withOpacity(0.5), Colors.black.withOpacity(0.0)],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.bottomLeft,
+                  padding: EdgeInsets.only(left: 25.0, bottom: 25.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        featuredOffers[index]['attributes']['merchant-name'],
+                        style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.w600),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        featuredOffers[index]['attributes']['title'],
+                        style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+                      ), 
+                    ],
+                  ),
+                ),
+              ],
             ),
           );
         },
